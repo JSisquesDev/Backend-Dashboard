b1a88cc3ccc5c109b0cb90dc363f848b0ce3a0a4
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2024-01-12 19:44:16 +0100

    Update logs

--- src/services/curriculum_vitae/controller.js
+++ src/services/curriculum_vitae/controller.js
@@ -1,9 +1,14 @@
 const model = require('./model');
 const logger = require('../../util/logger');
+const reqDetective = require('../../util/requestDetective');
 
 module.exports = {
   async getCurriculumVitae(req, res) {
     logger.info('controller.js - Entering getCurriculumVitae()');
+
+    const requestData = reqDetective.analize(req);
+
+    logger.debug('controller.js - Request data: ' + JSON.stringify(requestData));
 
     let language = req.params.LANGUAGE;
 
@@ -11,12 +16,17 @@ module.exports = {
       language = process.env.DEFAULT_LANGUAGE;
     }
 
+    logger.debug('controller.js - Selected language: ' + language);
+
     var result = await model.getCurriculumVitae(language);
 
     if (!result) {
-      return res.send('');
+      logger.error('controller.js - CV not obtained: ');
+      return res.status(400).send('');
     }
 
-    return res.send(result);
+    logger.debug('controller.js - Response: ' + JSON.stringify(result));
+
+    return res.status(200).send(result);
   },
 };
--- src/services/curriculum_vitae/model.js
+++ src/services/curriculum_vitae/model.js
@@ -10,25 +10,33 @@ module.exports = {
     const personalData = await dao.getPersonalData(language);
 
     if (!personalData) {
+      logger.error('model.js - Personal data not founded');
       return null;
     }
+
+    logger.debug('model.js - Personal data: ' + JSON.stringify(personalData));
 
     const socialMedia = await dao.getSocialMedia();
 
     if (!socialMedia) {
+      logger.error('model.js - Studies not founded');
       return null;
     }
 
     const studies = await dao.getStudies(language);
 
     if (!studies) {
+      logger.error('model.js - Studies not founded');
       return null;
     }
+
+    logger.debug('model.js - Studies: ' + JSON.stringify(studies));
 
     /*
     const experience = await dao.getStudies(language);
 
     if (!experience) {
+      logger.error('model.js - Studies not founded');
       return null;
     }
     */
@@ -36,44 +44,65 @@ module.exports = {
     const languages = await dao.getLanguages();
 
     if (!languages) {
+      logger.error('model.js - Languages not founded');
       return null;
     }
+
+    logger.debug('model.js - Languages: ' + JSON.stringify(languages));
 
     const projects = await dao.getProjects(language);
 
     if (!projects) {
+      logger.error('model.js - Projects not founded');
       return null;
     }
+
+    logger.debug('model.js - Projects: ' + JSON.stringify(projects));
 
     const programs = await dao.getPrograms();
 
     if (!programs) {
+      logger.error('model.js - Programs not founded');
       return null;
     }
+
+    logger.debug('model.js - Programs: ' + JSON.stringify(programs));
 
     const idioms = await dao.getIdioms(language);
 
     if (!idioms) {
+      logger.error('model.js - Idioms not founded');
       return null;
     }
+
+    logger.debug('model.js - Idioms: ' + JSON.stringify(idioms));
 
     const awards = await dao.getAwards(language);
 
     if (!awards) {
+      logger.error('model.js - Awards not founded');
       return null;
     }
+
+    logger.debug('model.js - Awards: ' + JSON.stringify(awards));
 
     const others = await dao.getOthers(language);
 
     if (!others) {
+      logger.error('model.js - Others not founded');
       return null;
     }
+
+    logger.debug('model.js - Others: ' + JSON.stringify(others));
 
     const aboutMe = await dao.getAboutMe(language);
 
     if (!aboutMe) {
+      logger.error('model.js - About me not founded');
       return null;
     }
+
+    logger.debug('model.js - About me: ' + JSON.stringify(aboutMe));
 
     const cv = {
       personalData: personalData,
